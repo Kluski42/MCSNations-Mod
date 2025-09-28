38657d4dd64e099e7c3bd9c780f6092d4a180383
Ashlyn <kluski42@gmail.com>
2025-09-27 21:01:35 -0400

    Made allays recognize TCC items as distinct from vanilla items.

--- src/main/java/net/wetnoodle/mcsnations/mixin/HappyGhastMixin.java
+++ src/main/java/net/wetnoodle/mcsnations/mixin/HappyGhastMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 public class HappyGhastMixin {
 
     @ModifyExpressionValue(method = "method_70699", at = @At(value = "INVOKE", target = "Ljava/util/function/Predicate;test(Ljava/lang/Object;)Z"))
-    private static boolean foodOrSnow$MCSNations(boolean original, ItemStack itemStack) {
+    private static boolean mcsNations$foodOrSnow(boolean original, ItemStack itemStack) {
         return original || itemStack.is(MCSNItemTags.HAPPY_GHAST_TRUE_TEMPT_ITEMS);
     }
 }
